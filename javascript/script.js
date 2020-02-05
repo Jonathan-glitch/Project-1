9d76c6b8f6b17e0deb6d28ca781887cb8269806a
elwdipath <jordan.kaiser@gmail.com>
2020-02-04 19:03:54 -0500

    Added google map API code and source script.

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,21 @@
 //Random User API
+var rUser;
 $.ajax({
     url: 'https://randomuser.me/api/',
     dataType: 'json',
     success: function(data) {
       console.log(data);
+      rUser = data
     }
   })
+
+  //Google Map
+  function initMap() {
+    // The location of Uluru
+    var uluru = {lat: -28.5383, lng: 81.3792};
+    // The map, centered at Uluru
+    var map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 4, center: uluru});
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({position: uluru, map: map});
+  }
