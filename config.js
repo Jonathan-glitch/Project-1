e73646fd06b727ed2096c129ba46b6934ba9421e
elwdipath <jordan.kaiser@gmail.com>
2020-02-11 18:19:16 -0500

    final push with config file

---
+++ config.js
@@ -0,0 +1,16 @@
+// var mAPI = {
+//     key: AIzaSyCf7tCxY_dvVr2h1sX5d_jZOfj7DQ952Uw,
+
+// }
+const rogerKey = "AIzaSyAdS-78ebQ58sfBJMSu6YeyVjt6pdmgYc4"
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAkDv4wf19KdFLTHAuy0nJKoyjEHcSjnCA",
+    authDomain: "swolemate-cf303.firebaseapp.com",
+    databaseURL: "https://swolemate-cf303.firebaseio.com",
+    projectId: "swolemate-cf303",
+    storageBucket: "swolemate-cf303.appspot.com",
+    messagingSenderId: "429965343031",
+    appId: "1:429965343031:web:3b573e5b40aed5802102b2"
+  };
+
